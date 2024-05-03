d5fa3b5bfce7f4c9dcfe63c9ecb2983bf02f612a
Abhishek <abhishekmech95@gmail.com>
2024-05-03 23:30:10 +0530

    Palindrome Numbers program added!

---
+++ mathematics/PalindromeNumbers.java
@@ -0,0 +1,22 @@
+package mathematics;
+
+public class PalindromeNumbers {
+    public static void main(String[] args) {
+        long input = 1221l;
+        boolean palindrome = isPalindrome(input);
+        System.out.println(palindrome);
+    }
+
+    public static boolean isPalindrome(long num){
+        long temp = num;
+        long reverse = 0l;
+
+        while(temp > 0){
+            long digit  = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp = temp / 10;
+        }
+
+        return num == reverse;
+    }
+}
