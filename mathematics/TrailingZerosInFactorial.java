e9ea0d719976cfc45d86e14766fa8a5168f8656c
Abhishek <abhishekmech95@gmail.com>
2024-05-05 17:08:36 +0530

    Trailing zeros in factorial added!!

---
+++ mathematics/TrailingZerosInFactorial.java
@@ -0,0 +1,49 @@
+package mathematics;
+
+public class TrailingZerosInFactorial {
+    public static void main(String[] args) {
+        int input = 5;
+
+        //naive method
+        int naiveMethod = naiveMethod(input);
+        System.out.println(naiveMethod);
+
+        //efficient method
+        int efficientMethod = efficientMethod(input);
+        System.out.println(efficientMethod);
+    }
+
+    //Time Complexity : Theta(n)
+    public static int naiveMethod(int num) {
+        int fact = 1;
+
+        for (int i = 2; i <= num; i++) {
+            fact = fact * i;
+        }
+
+        int count = 0;
+        while (fact % 10 == 0) {
+            count++;
+            fact = fact / 10;
+        }
+
+        return count;
+    }
+
+    /*
+    NEED TO COUNT 2 & 5 -> TO GET TRAILING ZEROS
+     */
+    public static int efficientMethod(int num) {
+        if (num < 5) {
+            return 0;
+        }
+
+        int result = 0;
+
+        for (int i = 5; i <= num; i = i * i) {
+            result = result + num / i;
+        }
+
+        return result;
+    }
+}
