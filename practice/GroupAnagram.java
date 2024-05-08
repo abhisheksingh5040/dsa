ad32a1460f9c728eb85a9e85cc933ab798374cd7
Abhishek <abhishekmech95@gmail.com>
2024-05-08 08:37:16 +0530

    group anagram program done

---
+++ practice/GroupAnagram.java
@@ -0,0 +1,32 @@
+package practice;
+
+import java.util.*;
+
+public class GroupAnagram {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("heart", "data", "listen", "silent", "enlist", "earth");
+        Collection<List<String>> result = groupAnagram(list);
+        System.out.println(result);
+
+    }
+
+    public static Collection<List<String>> groupAnagram(List<String> list) {
+        Map<String, List<String>> map = new TreeMap<>(Comparator.comparing(String::length).reversed());
+
+        for (String li : list) {
+            char[] charArray = li.toCharArray();
+            Arrays.sort(charArray);
+            String str = String.valueOf(charArray);
+            if (map.containsKey(str)) {
+                List<String> anagrams = map.get(str);
+                anagrams.add(li);
+                map.put(str,anagrams);
+            } else {
+                List<String> anagrams = new ArrayList<>();
+                anagrams.add(li);
+                map.put(str,anagrams);
+            }
+        }
+        return map.values();
+    }
+}
