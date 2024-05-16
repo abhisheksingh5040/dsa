5569a4d9c178d624b769bc3708d3379976695e0f
Abhishek <abhishekmech95@gmail.com>
2024-05-16 23:38:13 +0530

    prime program done with optimize way

--- mathematics/Prime.java
+++ mathematics/Prime.java
@@ -2,9 +2,15 @@ package mathematics;
 
 public class Prime {
     public static void main(String[] args) {
-        int input = 18;
+        int input = 19;
         boolean naive = naive(input);
         System.out.println(naive);
+
+        boolean efficientWay = efficientWay(input);
+        System.out.println(efficientWay);
+
+        boolean moreEfficient = moreEfficient(input);
+        System.out.println(moreEfficient);
     }
 
     public static boolean naive(int num) {
@@ -13,6 +19,35 @@ public class Prime {
 
         for (int i = 2; i < num; i++)
             if (num % i == 0)
+                return false;
+
+        return true;
+    }
+
+    //Time complexcity : O(root N)
+    public static boolean efficientWay(int num) {
+        if (num == 1)
+            return false;
+
+        for (int i = 2; i * i < num; i++)
+            if (num % i == 0)
+                return false;
+
+        return true;
+    }
+
+    public static boolean moreEfficient(int num) {
+        if (num == 1)
+            return false;
+
+        if (num == 2 || num == 3)
+            return true;
+
+        if (num % 2 == 0 || num % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i < num; i = i + 6)
+            if (num % i == 0 || num % (i + 2) == 0)
                 return false;
 
         return true;
