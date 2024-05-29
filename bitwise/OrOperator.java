d40912ae9eee745d80b3ca97ee60c709573a0b80
Abhishek <abhishekmech95@gmail.com>
2024-05-29 16:09:11 +0530

    Or Operator program added

---
+++ bitwise/OrOperator.java
@@ -0,0 +1,9 @@
+package bitwise;
+
+public class OrOperator {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 3;
+        System.out.println(x | y); // output: 7
+    }
+}
