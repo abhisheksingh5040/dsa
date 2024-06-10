c690cc4b1f67a739523a1664281815abc8a1b2f1
Abhishek <abhishekmech95@gmail.com>
2024-06-10 22:45:52 +0530

    program added

--- bitwise/XOROperator.java
+++ bitwise/XOROperator.java
@@ -4,6 +4,9 @@ public class XOROperator {
     public static void main(String[] args) {
         int x = 6;
         int y = 3;
+        int a = 2;
+        int b = 3;
         System.out.println(x ^ y); //output: 5
+        System.out.println(a ^ b); //output: 1
     }
 }
