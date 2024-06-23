11eef3f9b463f2b4afe604f3e57ceaabcb6f9654
Abhishek <abhishekmech95@gmail.com>
2024-06-23 20:48:29 +0530

    k size max sub array window added

---
+++ practice/KsizeMaximumSubArray.java
@@ -0,0 +1,32 @@
+package practice;
+
+public class KsizeMaximumSubArray {
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 1, 8, 2, 9, 1};
+        int k = 3;
+        int n = arr.length;
+        int maxedSubArray = maxSubArray(arr, k, n);
+        System.out.println(maxedSubArray);
+    }
+
+    public static int maxSubArray(int[] arr, int k, int n) {
+        if (n < k) {
+            return -1;
+        }
+
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        maxSum = sum;
+
+        for (int i = k; i < n; i++) {
+            sum += arr[i] - arr[i - k];
+            maxSum = Math.max(maxSum, sum);
+        }
+
+        return maxSum;
+    }
+}
