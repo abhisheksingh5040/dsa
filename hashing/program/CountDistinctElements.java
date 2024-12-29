a651df5f73eb056583462bd0c665d0d95886e1ba
Abhishek <abhishekmech95@gmail.com>
2024-12-29 12:28:24 +0530

    count dictinct element program added

---
+++ hashing/program/CountDistinctElements.java
@@ -0,0 +1,24 @@
+package hashing.program;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CountDistinctElements {
+
+    public static void main(String[] args) {
+        int[] input = {15, 12, 13, 12, 13, 13, 18};
+        int countDistinctElement = countDistinctElement(input, input.length);
+        System.out.println(countDistinctElement);
+    }
+
+    public static int countDistinctElement(int[] arr, int n) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (set.add(arr[i])) count++;
+        }
+
+        return count;
+    }
+}
