c1c09a4b98e2a29e861069e96d4ce2ba6d24cdc7
Abhishek <abhishekmech95@gmail.com>
2024-12-29 12:50:40 +0530

    union of two unsorted array program added

---
+++ hashing/program/UnionOfTwoUnsortedArray.java
@@ -0,0 +1,33 @@
+package hashing.program;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class UnionOfTwoUnsortedArray {
+    public static void main(String[] args) {
+        int[] a = {10, 15, 20, 25, 30, 50};
+        int[] b = {30, 5, 15, 80};
+
+        List<Integer> union = unionOfTwoUnsortedArray(a, b);
+        System.out.println(union);
+    }
+
+    public static List<Integer> unionOfTwoUnsortedArray(int[] a, int[] b) {
+        Set<Integer> result = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : a) {
+            set.add(num);
+        }
+
+        for (int num : b) {
+            if (!set.add(num)) {
+                result.add(num);
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+}
