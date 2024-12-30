8356bfe0ed054d3e8f86834b8ca93728fc2044b9
Abhishek <abhishekmech95@gmail.com>
2024-12-30 11:26:36 +0530

    zero sum problem added

---
+++ hashing/program/SubArrayWithZeroSum.java
@@ -0,0 +1,46 @@
+package hashing.program;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Time complexity : O(n)
+ * Space complexity : O(n)
+ */
+public class SubArrayWithZeroSum {
+
+    public static void main(String[] args) {
+        int[] input = {-3, 4, -3, -1, 1};
+
+        boolean result = subArrayWithZeroSum(input);
+        System.out.println(result);
+    }
+
+    /**
+     * int[] = {a,b,c,d,e};
+     *     a + b + c + d + e = totalSum
+     *     a + b + c = currentSum
+     *     d + e = zeroSum
+     *     so => totalSum = currentSum + zeroSum
+     */
+    public static boolean subArrayWithZeroSum(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        int currentSum = 0;
+
+        for (int num : arr) {
+            currentSum += num;
+
+            if (set.contains(currentSum)) {
+                return true;
+            }
+
+            if (num == 0) {
+                return true;
+            }
+
+            set.add(currentSum);
+        }
+
+        return false;
+    }
+}
