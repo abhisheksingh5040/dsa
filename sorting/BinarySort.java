e92a823d6b0135d3509729fe9a2dd59acc5786c6
Abhishek <abhishekmech95@gmail.com>
2025-06-03 10:11:56 +0530

    bubble sort program added

--- sorting/BinarySort.java
+++ sorting/BinarySort.java
@@ -4,24 +4,50 @@ import java.util.Arrays;
 
 public class BinarySort {
     public static void main(String[] args) {
-        int[] arr = {2, 1, 2, 6, 5, 3, 4, 8, 9, 6};
-        sort(arr);
+//        int[] arr = {3, 2, 5, 8, 7, 6, 1};
+        int[] arr = {3, 2, 5, 8};
+        int n = arr.length;
+        binarySort(arr,n);
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void sort(int[] arr) {
-        int n = arr.length;
+    /**
+     * Time complexity : Theta(N ^ 2)
+     *
+     */
+    public static void binarySort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if(arr[j] < arr[j + 1]){
+                    swap(arr,j,j + 1);
+                }
+            }
+        }
+    }
+
+    /**
+     * Time complexity : O(N ^ 2)
+     *
+     */
+    public static void binarySortOptimize(int[] arr, int n) {
         for (int i = 0; i < n - 1; i++) {
             boolean flag = false;
             for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                if(arr[j] < arr[j + 1]){
+                    swap(arr,j,j + 1);
                     flag = true;
                 }
             }
-            if (!flag) break;
+
+            if(!flag){
+                return;
+            }
         }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
