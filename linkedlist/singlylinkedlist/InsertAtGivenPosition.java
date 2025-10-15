9137af28c87219380db79e46b96f2c75cf33318d
Abhishek <abhishekmech95@gmail.com>
2025-10-15 16:02:35 +0530

    Insert at given position

--- linkedlist/singlylinkedlist/InsertAtGivenPosition.java
+++ linkedlist/singlylinkedlist/InsertAtGivenPosition.java
@@ -10,19 +10,19 @@ public class InsertAtGivenPosition {
             Node node = new Node(data);
             node.next = head;
             return node;
-        } else {
-            while (current != null) {
-                if (pos == count - 1) {
-                    Node node = new Node(data);
-                    node.next = current.next;
-                    current.next = node;
-                    return head;
-                } else {
-                    count++;
-                    current = current.next;
-                }
-            }
-            return head;
         }
+        while (current != null) {
+            if (pos == count - 1) {
+                Node node = new Node(data);
+                node.next = current.next;
+                current.next = node;
+                return head;
+            } else {
+                count++;
+                current = current.next;
+            }
+        }
+        return head;
+
     }
 }
